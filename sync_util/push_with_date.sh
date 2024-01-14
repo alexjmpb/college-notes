71927ddf7afaaf6e2253231472922da3245871a1
Alexander Mendoza <alexjmp.tests@gmail.com>
2024-01-14 16:30:27 -0500

    2024-01-14

--- sync_util/push_with_date.sh
+++ sync_util/push_with_date.sh
@@ -4,14 +4,15 @@
 export TZ=America/Bogota
 CURRENT_DATE=$(date +"%F")
 
+# squash all previous commits with the same name
+PREVIOUS_COMMITS=$(git log --pretty="%h" --grep=$CURRENT_DATE)
+COMMIT_COUNT=$(echo "$PREVIOUS_COMMITS" | wc -w)
+if [ "$COMMIT_COUNT" -gt 0 ]; then
+    git reset --soft HEAD~$COMMIT_COUNT
+fi
 # commit changes with the current date as the commit message
 git add .
 git commit -m "$CURRENT_DATE"
 
-# squash all previous commits with the same name
-PREVIOUS_COMMITS=$(git rev-list --all --grep="$CURRENT_DATE")
-git reset --soft HEAD~$(echo "$PREVIOUS_COMMITS" | wc -w)
-git commit -m "$CURRENT_DATE"
-
 # force push the changes
-git push --force
+git push --force origin HEAD:main
