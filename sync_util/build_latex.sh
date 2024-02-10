675957d0062238da2c9e99423ca7af34c743dbd1
Alexander Mendoza <alexjmp.tests@gmail.com>
2024-02-10 13:23:17 -0500

    2024-02-10

--- sync_util/build_latex.sh
+++ sync_util/build_latex.sh
@@ -2,5 +2,5 @@
 export FILE_PATH=$1
 export FILE_DIRNAME=$2
 cd $FILE_DIRNAME
-latexmk -pdf -outdir="$FILE_DIRNAME/build/" $FILE_PATH -synctex=1
-pdflatex -file-line-error -synctex=1 --output-directory="$FILE_DIRNAME/build/" $FILE_PATH
+latexmk -pdf -outdir="$FILE_DIRNAME/build/" $FILE_PATH -synctex=1 -interaction=nonstopmode -file-line-error
+pdflatex -file-line-error -synctex=1 --output-directory="$FILE_DIRNAME/build/" $FILE_PATH -interaction=nonstopmode
